f7fcd53ce68db5b3108adc290a55cba1bcb19d91
p-bradley <p_bradley@fanshaweonline.ca>
2019-09-26 13:43:46 -0400

    running node

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';  //local host
-const port = 3000; //a node conention
+const port = process.env.PORT 3000; //a node conention
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
